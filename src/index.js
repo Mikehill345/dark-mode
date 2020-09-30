945d25967cb083b85b48a6a673f50915ec6c736f
mike hill <michael-hill@lambdastudents.com>
2020-09-30 15:56:34 -0400

    finished mvp working on stretch

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import axios from "axios";
-
 import Charts from "./components/Charts";
 import Navbar from "./components/Navbar";
 
@@ -9,7 +8,6 @@ import "./styles.scss";
 
 const App = () => {
   const [coinData, setCoinData] = useState([]);
-  const [darkMode, setDarkMode] = useState(false);
 
   useEffect(() => {
     axios
@@ -20,8 +18,8 @@ const App = () => {
       .catch(err => console.log(err));
   }, []);
   return (
-    <div className={darkMode ? "dark-mode App" : "App"}>
-      <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
+    <div className="App">
+      <Navbar  />
       <Charts coinData={coinData} />
     </div>
   );
